2ffcd7a5239701fa240b47dc917cb8b31c6dab7a
vigneshgopal97 <gvigneshcse97@gmail.com>
2025-03-01 14:14:50 +0530

    vignesh_1 fixes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 const Router = () => {
     return (
         <div>
